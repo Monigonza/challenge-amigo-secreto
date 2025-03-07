633b0308c268eb2920709bde7007f47c8faa7aac
Monigonza <monica.lahibeth@gmail.com>
2025-03-07 14:17:28 -0300

    Actualiza app.js
    Se declara la variable amigos xomo arreglo.
    Se crea la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Declaración de la variable amigo como arreglo
+let amigos = [];
+
+//Función para agregar un amigo al arreglo y que sea diferente a vacío
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+        console.log(amigos);
+
+    if(!nombreAmigo){
+        alert('Debes ingresar un nombre');
+        return;
+    }
+  amigos.push(nombreAmigo);
+  inputAmigo.value = '';
+  inputAmigo.focus();
+  console.log(amigos);
+}
